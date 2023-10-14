0a7f9f2c8dfd9c99076233fdbc820e06c9a9302f
Marti Paredes <martiparedes19@gmail.com>
2023-10-14 09:51:33 +0200

    Finished drivers nationality chart

--- components/charts/DriversNationality.vue
+++ components/charts/DriversNationality.vue
@@ -1,13 +1,14 @@
 <template>
-  <Doughnut ref="canvas" :options="options"  :data="chartData" />
+  <Doughnut ref="canvas" :options="options" :data="chartData" />
 </template>
 
 <script setup>
 import { ref, onMounted, computed } from 'vue';
-import { Chart as ChartJS, ArcElement, Tooltip, Legend,  } from 'chart.js'
-import { Doughnut } from 'vue-chartjs'
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
+import { Doughnut } from 'vue-chartjs';
 
-ChartJS.register(ArcElement, Tooltip, Legend)
+ChartJS.register(ArcElement, Tooltip, Legend);
+
 const rawData = ref(null);
 
 const chartData = computed(() => {
@@ -23,20 +24,41 @@ const chartData = computed(() => {
         label: 'Numero',
         data: data ? data.map((driver) => driver.count) : [],
         backgroundColor: colors,
-      }
-    ]
+      },
+    ],
   };
 });
 
-const options =  {
-    plugins: {
-      legend: {
-        responsive: true,
-        position: 'bottom',
+const options = {
+  plugins: {
+    legend: {
+      responsive: true,
+      position: 'bottom',
+    },
+    tooltip: {
+      callbacks: {
+        footer: function (context) {
+              let footer = ""
+
+              if (context[0].parsed !== null) {
+                const dataIndex = context[0].dataIndex;
+                footer = `${rawData.value[dataIndex].drivers.join(', ')}`;
+              }
+              
+              return footer;
+            },
+            labelColor: function (context) {
+              return {
+                borderColor: 'rgba(0, 0, 0, 0)',
+                backgroundColor: context.dataset.backgroundColor[context.dataIndex],
+                borderRadius: 6,
+              };
+            },
       },
-    }
-  }
-  
+    },
+  },
+};
+
 onMounted(async () => {
   const data = await fetchData();
   rawData.value = data;
@@ -45,7 +67,7 @@ onMounted(async () => {
 function generateDynamicColors(numColors) {
   const dynamicColors = [];
   for (let i = 0; i < numColors; i++) {
-    const hue = (i * 40) % 360; // Ajusta el valor de desplazamiento para obtener diferentes tonalidades
+    const hue = (i * 40) % 360;
     dynamicColors.push(`hsla(${hue}, 80%, 80%, 0.8)`);
   }
   return dynamicColors;
@@ -53,24 +75,30 @@ function generateDynamicColors(numColors) {
 
 async function fetchData() {
   try {
-    const { data: response } = await useFetch('https://ergast.com/api/f1/current/drivers.json?limit=1000');
-    const drivers = response._rawValue.MRData.DriverTable.Drivers;
+    const response = await fetch('https://ergast.com/api/f1/current/drivers.json?limit=1000');
+    const { MRData: { DriverTable: { Drivers } } } = await response.json();
 
-    const nationalityCounts = drivers.reduce((counts, driver) => {
+    const nationalityCounts = Drivers.reduce((counts, driver) => {
       const nationality = driver.nationality;
 
       if (!counts[nationality]) {
-        counts[nationality] = 0;
+        counts[nationality] = {
+          count: 0,
+          drivers: [],
+        };
       }
 
-      counts[nationality]++;
+      counts[nationality].count++;
+
+      counts[nationality].drivers.push(`${driver.givenName} ${driver.familyName}`);
 
       return counts;
     }, {});
 
-    return Object.entries(nationalityCounts).map(([name, count]) => ({
+    return Object.entries(nationalityCounts).map(([name, { count, drivers }]) => ({
       name,
-      count
+      count,
+      drivers,
     }));
   } catch (error) {
     console.error('Error fetching Drivers Nationality: ' + error);
