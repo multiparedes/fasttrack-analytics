6b83154757401b6b20b12ac71a07b044ead1f50c
Marti Paredes <martiparedes19@gmail.com>
2023-12-14 20:55:32 +0100

    Custom 404

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,11 +3,10 @@
         class="w-full py-1 h-8 bg-white dark:bg-slate-700 text-center text-sm inline-flex items-center gap-2 justify-center dark:text-gray-100">
         Made by Martí Paredes
         <div class="flex gap-2 dark:text-gray-100">
-            <Button class="w-4 " color="transparent" :icon="{ prefix: 'fab', iconName: 'github' }"
+            <Button class="w-4 hover:text-primary-500" color="transparent" :icon="{ prefix: 'fab', iconName: 'github' }"
                 link="https://github.com/multiparedes" is-external-link></Button>
-            <Button class="w-4" color="transparent" :icon="{ prefix: 'fab', iconName: 'linkedin' }"
+            <Button class="w-4 hover:text-primary-500" color="transparent" :icon="{ prefix: 'fab', iconName: 'linkedin' }"
                 link="https://www.linkedin.com/in/martiparedessalom/" is-external-link></Button>
         </div>
-
     </div>
 </template>
---
+++ error.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="bg-slate-300 dark:bg-slate-500 flex flex-col relative min-h-screen"
+        style="font-family: 'Barlow', sans-serif;">
+        <NavBar />
+        <main class="flex-grow flex items-center justify-center">
+            <section class="grid gap-4 py-4  mx-4 w-full max-w-screen-xl place-content-center text-center">
+                <Card>
+                    <p class="font-bold italic  text-3xl">
+                        &lt 404 &gt
+                    </p>
+                    <h1 class="text-xl">Ugh, seems that something broke...</h1>
+                    <p class="flex gap-2 justify-center">We are sorry</p>
+
+                    <div class="flex justify-center gap-6 mt-4">
+                        <Button color="secondary" variant="bordered" :icon-start="true"
+                            :icon="{ prefix: 'fas', iconName: 'arrow-left-long' }" @click="$router.go(-1)">Go back</Button>
+                        <Button :icon="{ prefix: 'fas', iconName: 'home' }" link="/">Home</Button>
+                    </div>
+                </Card>
+            </section>
+        </main>
+        <Footer />
+    </div>
+</template>
+
+<script setup>
+import Card from '~/components/Card.vue';
+import Button from './components/Button.vue';
+</script>
